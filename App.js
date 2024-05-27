3a7782453642683ff4c8d85dd029a232fec1a3f6
CassGH <pcforson-coleman@st.ug.edu.gh>
2024-05-27 16:15:17 +0000

    Increased the font size of text to 24

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My name is Forson-Coleman</Text>
+      <Text style={styles.text}>My name is Forson-Coleman</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +17,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 24,
+  }
 });
